aa58c99a34f7521db5a957bf55d5082e866034e0
emma31-dev <emmanuelfidel07@gmail.com>
2026-04-03 10:58:00 +0100

    fix: removed println bloat

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,15 +5,8 @@ use std::{env};
 use short_commands::{run_command, if_exists};
 
 fn main() {
-    println!("===============================");
-    println!("   Welcome to short commands   ");
-    println!("===============================");
-    
     let arg: Vec<String> = env::args().collect();
-    
     let (command, command_arg1, command_arg2 ) = if_exists(arg);
-    
-    println!("Short command running..");
     
     run_command(command, command_arg1, command_arg2);
 }
